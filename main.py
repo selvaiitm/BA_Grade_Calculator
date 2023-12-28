3485fc001203bb7b7b7ec4cec78527efa34410d6
selvaiitm <96175511+selvaiitm@users.noreply.github.com>
2023-12-28 21:28:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.text('')
 
 st.markdown("<hr>", unsafe_allow_html=True)
 st.markdown("""
-<h1 align="center">  TDS Grade Calculator </h1>
+<h1 align="center">  BA Grade Calculator </h1>
 """, unsafe_allow_html=True)
 
 st.markdown("<hr>", unsafe_allow_html=True)
@@ -17,7 +17,7 @@ st.markdown("""
 st.markdown("<hr>", unsafe_allow_html=True)
 
 st.markdown("""
-<h3 align="center"> Welcome to the TDS Grade Calculator app, this app is created  for calculating the final grade for TDS of september-2023 term, a Diploma level course for  BS IN DATA SCIENCE COURSE OFFERED BY IIT MADRAS 
+<h3 align="center"> Welcome to the BA Grade Calculator app, this app is created  for calculating the final grade for BA of september-2023 term, a Diploma level course for  BS IN DATA SCIENCE COURSE OFFERED BY IIT MADRAS 
 """, unsafe_allow_html=True)
 
 
@@ -25,10 +25,25 @@ st.markdown("""
 st.markdown("<hr>", unsafe_allow_html=True)
 
 
-# Final course score T = 0.1GAA + 0.2 ROE1 + 0.2 P1 + 0.2P2 + 0.3F
+
 
 def score_calculator(a,b,c,d,e):
-  return 0.1*a + 0.2*b + 0.2*c + 0.2*d + 0.3*e
+
+  assignment = 0.4*a
+
+  
+  # Quiz Marks = 0.7*Max(Qz1, Qz2) + 0*3Min(Qz1, Qz2)
+  
+  quiz = 0.14*max(b,c) + 0.6*min(b,c)
+  
+  end_term = min(e*.45,0.4)
+  
+  total_score = quiz+assignment+endterm+d
+  if total_score >100:
+    total_score = 100
+  return total_score
+
+
 
 
 def Grade_calculator(total_score):
@@ -54,17 +69,18 @@ def Grade_calculator(total_score):
   else:
     return 'F'
 
+st.markdown(""" <h5 align = "center"> Enter all the scores as visible in the dashboard """, unsafe_allow_html=True)
 
-st.markdown(""" <h5 align = "center">Enter the Graded assignment score:</h5>""", unsafe_allow_html=True)
+st.markdown(""" <h5 align = "center">Enter the Average  assignment for final score :</h5>""", unsafe_allow_html=True)
 a = st.number_input(label = " " , key="first_digit_input") 
 
-st.markdown(""" <h5 align = "center">Enter the Programing quiz score:</h5>""", unsafe_allow_html=True)
+st.markdown(""" <h5 align = "center">Enter the quiz one score don't enter zero if absent:</h5>""", unsafe_allow_html=True)
 b = st.number_input(label = " " , key="second_digit_input")  # Using an empty string as label
 
-st.markdown(""" <h5 align = "center">Enter the Course project one score:</h5>""", unsafe_allow_html=True)
+st.markdown(""" <h5 align = "center">Enter the quiz two score don't enter zero with absent:</h5>""", unsafe_allow_html=True)
 c = st.number_input(label = " " , key="third_digit_input")  # Using an empty string as label
 
-st.markdown(""" <h5 align = "center">Enter the Course project two score:</h5>""", unsafe_allow_html=True)
+st.markdown(""" <h5 align = "center">Enter the bonus marks:</h5>""", unsafe_allow_html=True)
 d = st.number_input(label = " " , key="fourth_digit_input")  # Using an empty string as label
 
 st.markdown(""" <h5 align = "center">Enter the Endterm Exam score:</h5>""", unsafe_allow_html=True)
@@ -74,17 +90,26 @@ Total_score = score_calculator(a,b,c,d,e)
 
 grade = Grade_calculator(Total_score)
 
+end_term = min(e*.45,0.4)
 
 
 if st.button('Start Calculating!'):
+
+  st.markdown(f"""
+  <h3 align="center">Your End_Term_Score capped to 100 : {end_term:.2f} 
+  """, unsafe_allow_html=True)
   
   st.markdown(f"""
   <h3 align="center">Your Total Score : {Total_score:.2f} 
   """, unsafe_allow_html=True)
   st.markdown(f"""
-  <h3 align="center">Your Grade for TDS is : {grade} 
+  <h3 align="center">Your Grade for BA is : {grade} 
   """, unsafe_allow_html=True)
+
+st.markdown("<hr>", unsafe_allow_html=True)
 
 st.markdown("""
 <h2 align="center"> Created by N.Tamilselvan - 21f2001270</h2>
 """, unsafe_allow_html=True)
+
+st.markdown("<hr>", unsafe_allow_html=True)
